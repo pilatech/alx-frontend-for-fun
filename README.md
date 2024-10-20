aa193ba79bc4e824b7fbfcabeb2a371c91587b82
Pilate <pilatech6@gmail.com>
2024-10-20 07:28:04 +0200

    do the scaffoling for markdown2html

---
+++ README.md
@@ -0,0 +1,1 @@
+**Optional Frontend Tasks**
---
+++ markdown2html.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Script that converts markdown to html"""
+
+
+if __name__ == "__main__":
+    import sys
+    import os
+
+    if len(sys.argv) < 3:
+        print('Usage: ./markdown2html.py README.md README.html',
+              file=sys.stderr)
+        sys.exit(1)
+
+    if not os.path.exists(sys.argv[1]):
+        print(f'Missing {sys.argv[1]}', file=sys.stderr)
+        sys.exit(1)
+
+    sys.exit(0)
